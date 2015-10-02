78e83349805ca9aaea92a9fad9be4261ec74eefa
justinhuynh <jhuynh@gmail.com>
2015-10-01 22:19:32 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,39 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '4.2.4'
+gem 'pg'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'devise'
+gem 'foundation-rails'
+gem 'pry-rails'
+gem 'faker'
+gem 'kaminari'
+gem 'fog'
+gem 'fog-aws'
+gem 'dotenv-rails'
+
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'launchy'
+  gem 'factory_girl'
+  gem 'valid_attribute'
+  gem 'shoulda-matchers', require: false
+end
+
+group :test do
+  gem 'coveralls', require: false
+  gem 'poltergeist'
+  gem 'database_cleaner'
+  gem 'phantomjs', require: 'phantomjs/poltergeist'
+end
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,27 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'shoulda-matchers'
+require File.join(File.dirname(__FILE__), 'support/valid_attribute')
+require File.join(File.dirname(__FILE__), 'support/factory_girl')
+require File.join(File.dirname(__FILE__), 'support/sign_in')
+require File.join(File.dirname(__FILE__), 'support/database_cleaner')
+require 'database_cleaner'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+require 'pry'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.infer_spec_type_from_file_location!
+end
