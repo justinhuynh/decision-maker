645c907e755abb70a13e93d935960bf91983bb2f
justinhuynh <jhuynh@gmail.com>
2015-10-03 13:49:37 -0400

    fixed shoulda-matchers issue

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 describe Question do
-
+  describe "validations" do
+    it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_most(140) }
+  end
 end
