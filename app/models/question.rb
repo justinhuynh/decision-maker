c3870345487c35abff342a299b74ff9ba0059df1
justinhuynh <jhuynh@gmail.com>
2015-10-04 13:13:18 -0400

    updated hound errors

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ActiveRecord::Base
   validates :body, presence: { message: "Question can't be blank" }
-  validates :body, length: { maximum: 140, message: "Question must be less than 140 characters" }
+  validates :body, length: {
+    maximum: 140,
+    message: "Question must be less than 140 characters"
+  }
 end
