5fee0af624a76da36198a8dfc3314738433fe028
justinhuynh <jhuynh@gmail.com>
2015-10-09 13:12:14 -0400

    rollback non-styling edits

--- app/models/choice_set.rb
+++
@@ -1,7 +0,0 @@
-class ChoiceSet < ActiveRecord::Base
-  belongs_to :question
-  belongs_to :choice
-
-  validates :question, presence: true
-  validates :choice, presence: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009162432) do
+ActiveRecord::Schema.define(version: 20151006212925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "choice_sets", force: :cascade do |t|
-    t.integer  "question_id", null: false
-    t.integer  "choice_id",   null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
 
   create_table "choices", force: :cascade do |t|
     t.string  "title"
