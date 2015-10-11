c9f2bfbe7207ddf81ec769ccf62624ae52f5473f
justinhuynh <jhuynh@gmail.com>
2015-10-11 16:46:49 -0400

    updated submit response to redirect home

---
+++ spec/features/user_views_saved_questions_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+feature "authenticated user views saved questions", %{
+  As an authenticated user
+  I would like to see my previous questions and responses
+  So I can understand how I responded to this question in the past
+
+  Acceptance Criteria:
+  - [ ] If I am authenticated when entering a question, my questions and
+        responses will be saved
+  - [ ] I can view my past questions, responses, recommendations and outcomes
+} do
+  context "Authenticated user signs in" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    scenario "and has link to dashboard" do
+    #   sign_in(user)
+    #   within("nav#top-bar") do
+    #     click_link "Dashboard"
+    #   end
+    #   expect(page).to have_content("#{user.first_name}'s Dashboard")
+    end
+  end
+end
