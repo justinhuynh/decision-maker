258259080f41343ada3ab7821ce0b71679c41482
justinhuynh <jhuynh@gmail.com>
2015-10-14 11:38:43 -0400

    build custom es client and add method template

---
+++ app/models/elastic_custom_client.rb
@@ -0,0 +1,19 @@
+require 'multi_json'
+require 'faraday'
+require 'elasticsearch/api'
+
+class ElasticCustomClient
+  include Elasticsearch::API
+
+  CONNECTION = ::Faraday::Connection.new url: ENV["ELASTICSEARCH_URL"]
+
+  def perform_request(method, path, params, body)
+    puts "--> #{method.upcase} #{path} #{params} #{body}"
+
+    CONNECTION.run_request \
+      method.downcase.to_sym,
+      path,
+      ( body ? MultiJson.dump(body): nil ),
+      {'Content-Type' => 'application/json'}
+  end
+end
